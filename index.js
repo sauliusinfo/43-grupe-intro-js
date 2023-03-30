5307a103db5656004ce69e8e526ab6c2a2caf6b2
Saulius <saulius.j@icloud.com>
2023-03-30 18:07:32 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-console.log(42);
-console.log(41);
-console.log(40);
-console.log(39);
-console.log(38);
+console.clear();
+
+const vardas = 'Saulius';
+const pabaigaVardo = '!';
+const printText = 'Labas rytas ' + vardas + pabaigaVardo;
+console.log(printText);
+
+const n1 = 4; // pvz: kai n1 yra text, n1 = '4'
+const n2 = 5;
+
+if (isNaN(n1)) {
+    console.log('Jei n1 nera sk., tai suma: ', n1 + n2);
+} else {
+    console.log('Suma: ', n1 + n2)
+}

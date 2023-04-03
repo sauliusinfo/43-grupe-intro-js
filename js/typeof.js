a6e0f975ff5c7a0b57562326dfd5bf4224108b9e
Saulius <saulius.j@icloud.com>
2023-04-03 21:18:25 +0300

    typeof

---
+++ js/typeof.js
@@ -0,0 +1,18 @@
+console.clear();
+
+const x = 42;
+let x1 = 41;
+var x2 = 40;
+
+const y = "text";
+const z = true;
+const obj = {};
+let func = function() {};
+
+console.log(x + ' - ' + typeof x); 
+console.log(x1, `-`, typeof x1); 
+console.log(`${x2} - ${typeof x2}`); 
+console.log(`y - ${typeof y}`); 
+console.log(`z - ${typeof z}`); 
+console.log(`obj - ${typeof obj}`); 
+console.log(`f-ja : ${typeof func}`);

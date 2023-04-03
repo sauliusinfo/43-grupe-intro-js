006e5841877233f1096e98391b9f0bf5c27c2a7a
Saulius <saulius.j@icloud.com>
2023-04-03 21:19:42 +0300

    array

---
+++ js/array.js
@@ -0,0 +1,8 @@
+console.clear();
+
+const pazymiai=[3,6,10];
+console.log(`Pazymiai: ${pazymiai}`);
+console.log(`Pazymys pirmas: `, pazymiai.length - 1);
+console.log(`Pazimys pirmas: ${pazymiai.length - 1}`);
+console.log(`Pazymys paskutinis: `, pazymiai[pazymiai.length - 1]);
+console.log(`Pazymys paskutinis: ${pazymiai[pazymiai.length - 1]}`);

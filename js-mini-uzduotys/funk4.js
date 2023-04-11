650ca1eb1b89085d4ca3e0b653bc70d6f707b0e3
Saulius <saulius.j@icloud.com>
2023-04-11 16:40:27 +0300

    funk4

--- js-mini-uzduotys/funk4.js
+++ js-mini-uzduotys/funk4.js
@@ -24,3 +24,25 @@ console.log(':: Funkcijos ::');
 //        vii. console.log( didziausiasSkaiciusSarase( [] ) );
 //              1. rezultatas: “Pateiktas sąrašas negali būti tuščias.”
 console.log('-- 4.a.b.c.d.e.f.g. --');
+
+function didziausiasSkaiciusSarase(sk) {
+    let a;
+    if (Array.isArray(sk) && sk.length !== 0) {
+        a = Math.max(...sk);
+    } else if (sk.length === 0) {
+        return 'Pateiktas sąrašas negali būti tuščias.';
+    } else {
+        return 'Pateikta netinkamo tipo reikšmė.';
+    }
+    return a
+}
+
+// let a;
+
+console.log( didziausiasSkaiciusSarase( [ 1 ] ) );
+console.log( didziausiasSkaiciusSarase( [ 1, 2, 3 ] ) );
+console.log( didziausiasSkaiciusSarase( [ -5, 78, 14, 0, 18 ] ) );
+console.log( didziausiasSkaiciusSarase( [ 69, 69, 69, 69, 66 ] ) );
+console.log( didziausiasSkaiciusSarase( [ -1, -2, -3, -4, -5, -6, -7, -8 ] ) );
+console.log( didziausiasSkaiciusSarase( "pomidoras" ) );
+console.log( didziausiasSkaiciusSarase( [] ) );
